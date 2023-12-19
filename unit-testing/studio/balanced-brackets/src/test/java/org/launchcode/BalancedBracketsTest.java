cfd7f9aa8357e5f014130066e3fd2c4c74fc7e13
Teena Dubey <avidubey2k5@gmail.com>
2023-12-18 19:46:28 -0600

    unittesting

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -8,6 +8,59 @@ class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
+
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void stringWithBracketsAndCharactersReturnsTrue(){
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a b c]"));
+    }
+    @Test
+    public void stringWithOpenBracketAndCharactersReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[abc"));
+    }
+    @Test
+    public void stringWithCloseBracketAndCharactersReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("abc]"));
+    }
+    @Test
+    public void onlyOppositeBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void onlyBracketsWithMultipleCharactersReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]abc"));
+
+
+    }
+    @Test
+    public void multipleCharactersWithOpenBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("abc["));
+    }
+    @Test
+    public void multipleCharactersWithOnlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("abc[]"));
+    }
+    @Test
+    public void moreOpenBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]"));
+    }
+    @Test
+    public void moreCloseBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]["));
+    }
+    @Test
+    public void onlyBracketsWithMultipleCharactersReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]abc"));
+    }
+    @Test
+    public void multipleCharactersWIthMultipleBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("ab[]cd[]"));
     }
 }
