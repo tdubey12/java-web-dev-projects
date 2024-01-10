2bad4bdfa15d3d59e9e3fc1bdb7e631c8a8ae591
Teena Dubey <avidubey2k5@gmail.com>
2024-01-09 19:32:07 -0600

    exception exercise

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,27 +1,72 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) {
         // Test out your Divide() function!
+
+        try {
+            System.out.println("Divide"+Divide(2,0));
+        } catch (Exception e) {
+            e.printStackTrace();
+            //throw new RuntimeException(e);
+        }
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Teena" , "java.Program");
 
         // Test out your CheckFileExtension() function!
+        HashMap<String, Integer> studentFileScore =new HashMap<>();
+
+        for(Map.Entry<String, String> student: studentFiles.entrySet()){
+            int score =-1;
+            try{
+                score = CheckFileExtension(student.getValue());
+
+            }catch(IllegalArgumentException ie){
+                System.out.println(student.getKey() +" "+ie.getMessage());
+                ie.printStackTrace();
+            }
+            studentFileScore.put(student.getKey(),score);
+        }
+
+        for(Map.Entry<String, Integer> student:studentFileScore.entrySet()){
+            System.out.println(student.getKey()+student.getValue());
+        }
+
     }
 
-    public static void Divide(int x, int y)
+    public static double Divide(int x, int y)
     {
-        // Write code here!
+       if(y==0) {
+           try{
+                throw new ArithmeticException("Cannot divide by 0");
+           }catch(ArithmeticException ae){
+               ae.printStackTrace();
+               throw new RuntimeException(ae);
+           }
+
+       }
+       return x/y;
     }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        if(fileName==null || fileName.isEmpty()){
+         throw new IllegalArgumentException("fileName is empty or null");
+        }
+        if(fileName.endsWith(".java")) {
+            return 1;
+        }else{
+            return 0;
+        }
+
     }
 }
