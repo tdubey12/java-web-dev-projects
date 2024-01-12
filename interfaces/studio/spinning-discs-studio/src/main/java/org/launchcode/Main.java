5ba524a85fed5e2262c1e8b75d915f5914568705
Teena Dubey <avidubey2k5@gmail.com>
2024-01-11 19:49:38 -0600

    studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args){
 
         CD cd = new CD("CD Example", 700, "CD-R", 350);
-        DVD dvd = new DVD("DVD Example", 4700, "DVD-R", 1450);
+        DVD dvd = new DVD("DVD Example1", 4700, "DVD-R", 1450);
 
         cd.spinDisc();
-        dvd.spinDisc();
+git comm        dvd.spinDisc();
 
         cd.readData();
         dvd.readData();
 
         System.out.println(cd.writeData(275));
-        System.out.println(dvd.writeData(8000));
+        System.out.println(dvd.writeData(5000));
 
         System.out.println(cd.diskInfo());
         System.out.println(dvd.diskInfo());
